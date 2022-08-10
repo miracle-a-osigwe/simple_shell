2368ff6d16406271446af075357908826391460e
Egberongbe-Amir <64702532+Egberongbe-Amir@users.noreply.github.com>
2022-08-10 15:41:53 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -117,57 +117,32 @@ char *_itoa(int num)
 int create_error(char **args, int err)
 
 {
-
-	char *error;
-
-	switch (err)
-
-	{
-
-	case -1:
-
-		error = error_env(args);
-
-		break;
-
-	case 1:
-
-		error = error_1(args);
-
-		break;
-
-	case 2:
-
-		if (*(args[0]) == 'e')
-
-			error = error_2_exit(++args);
-
+char *error;
+switch (err)
+{
+case -1:
+error = error_env(args);
+break;
+case 1:
+error = error_1(args);
+break;
+case 2:
+if (*(args[0]) == 'e')
+error = error_2_exit(++args);
 else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
-
-			error = error_2_syntax(args);
-
-		else
-
-			error = error_2_cd(args);
-
-		break;
-
-	case 126:
-		error = error_126(args);
-
-		break;
-
-	case 127:
-
-		error = error_127(args);
-
-		break;
-
-	}
-	write(STDERR_FILENO, error, _strlen(error));
-
-	if (error)
-		free(error);
-	return (err);
-
+error = error_2_syntax(args);
+else
+error = error_2_cd(args);
+break;
+case 126:
+error = error_126(args);
+break;
+case 127:
+error = error_127(args);
+break;
+}
+write(STDERR_FILENO, error, _strlen(error));
+if (error)
+free(error);
+return (err);
 }
