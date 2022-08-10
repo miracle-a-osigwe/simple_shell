9d2372ea37ad2b02d147978a8302dbe2dc0e2c9f
Egberongbe-Amir <64702532+Egberongbe-Amir@users.noreply.github.com>
2022-08-10 16:29:58 +0100

    Update locate.c

--- locate.c
+++ locate.c
@@ -102,95 +102,50 @@ char *get_location(char *command)
  */
 
 char *fill_path_dir(char *path)
-
 {
+int i, length = 0;
+char *path_copy, *pwd;
 
-	int i, length = 0;
-
-	char *path_copy, *pwd;
-
-
-
-	pwd = *(_getenv("PWD")) + 4;
-
-	for (i = 0; path[i]; i++)
-
-	{
-
-		if (path[i] == ':')
-
-		{
-
+pwd = *(_getenv("PWD")) + 4;
+for (i = 0; path[i]; i++)
+{
+if (path[i] == ':')
+{
 if (path[i + 1] == ':' || i == 0 || path[i + 1] == '\0')
-
-				length += _strlen(pwd) + 1;
-
-			else
-
-				length++;
-
-		}
-
-		else
-
-			length++;
-
-	}
-
-	path_copy = malloc(sizeof(char) * (length + 1));
-
-	if (!path_copy)
-
-		return (NULL);
-
-	path_copy[0] = '\0';
-
-	for (i = 0; path[i]; i++)
-
-	{
-
-		if (path[i] == ':')
-
-		{
-
-			if (i == 0)
-
-			{
-
-				_strcat(path_copy, pwd);
-
-				_strcat(path_copy, ":");
-
-			}
-
-			else if (path[i + 1] == ':' || path[i + 1] == '\0')
-
-			{
-
-				_strcat(path_copy, ":");
-
-				_strcat(path_copy, pwd);
-
-			}
-
-			else
-
-				_strcat(path_copy, ":");
-
-		}
-
-		else
-
-		{
-
-			_strncat(path_copy, &path[i], 1);
-
-		}
-
-	}
-
-	return (path_copy);
-
+length += _strlen(pwd) + 1;
+else
+length++;
+}
+else
+length++;
+}
+path_copy = malloc(sizeof(char) * (length + 1));
+if (!path_copy)
+return (NULL);
+path_copy[0] = '\0';
+for (i = 0; path[i]; i++)
+{
+if (path[i] == ':')
+{
+if (i == 0)
+{
+_strcat(path_copy, pwd);
+_strcat(path_copy, ":");
+}
+else if (path[i + 1] == ':' || path[i + 1] == '\0')
+{
+_strcat(path_copy, ":");
+_strcat(path_copy, pwd);
+}
+else
+_strcat(path_copy, ":");
+}
+else
+{
+_strncat(path_copy, &path[i], 1);
+}
+}
+return (path_copy);
 }
 
 
