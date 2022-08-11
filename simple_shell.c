02afabaae7417a890eda5a127cdb32feb44d8346
Egberongbe-Amir <64702532+Egberongbe-Amir@users.noreply.github.com>
2022-08-11 14:18:58 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -30,11 +30,11 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 	unsigned int is_pipe = 0;
 
 	vars_t vars = {NULL, NULL, 0, NULL, 0, NULL, NULL, NULL, NULL};
-	
+
 	vars.argv = argv;
-	
+
 	vars.env = make_enviroment(environment);
-	
+
 	signal(SIGINT, sig_handler);
 
 	if (!isatty(STDIN_FILENO))
